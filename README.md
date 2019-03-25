cd9b3a264252fa92a6f9906823e8f5413ff2f9b7
Nunez, Yoel <y.nunez@floms.com>
2019-03-24 22:45:44 -0500

    updated build scripts and package.json

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 # Prerequisites
 
 `npm install`
+`npm install gulp -g`
 
 # Building the application
 
-`npm run build`
+`gulp build-app` or `gulp package`
 
 # Running the application
 
-`npm run app`
+`npm run app` or navigate to `dist/app/` and open the executable
+s
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
 const {series, task, src, dest} = require('gulp');
 const {spawn} = require('child_process');
 const createDMG = require('electron-installer-dmg');
+const electronInstaller = require('electron-winstaller');
+
+const isWindows = () => process.platform === 'win32';
 
 const DIST_PATH = 'dist';
 const APP_DIST_PATH = `${DIST_PATH}/postignis`;
 
 const packager = require('electron-packager');
 
-const copy = () => {
-  return src([
-    'package.json',
-    'index.js'
-  ]).pipe(dest(APP_DIST_PATH))
-};
-
-const build = (done) => {
-  const runBuild = spawn('npm', ['run', 'build', '--', '--prod']);
+task('build-ui', (done) => {
+  const runBuild = spawn(isWindows() ? 'npm.cmd' : 'npm', ['run', 'build', '--', '--prod']);
 
   runBuild.stdout.on('data', function (data) {
     console.log(data.toString());
@@ -29,9 +25,16 @@ const build = (done) => {
     // console.log('child process exited with code ' + code.toString());
     done();
   });
-};
+});
 
-const pack = async () => {
+task('copy-resources', () => {
+  return src([
+    'package.json',
+    'index.js'
+  ]).pipe(dest(APP_DIST_PATH))
+});
+
+task('package-app', async () => {
   return await packager({
     dir: APP_DIST_PATH,
     out: `${DIST_PATH}/app`,
@@ -42,32 +45,36 @@ const pack = async () => {
     appCategoryType: 'public.app-category.developer-tools',
     appBundleId: 'com.floms.postignis'
   });
-};
+});
 
-const installer = async () => {
-  const mac = new Promise((resolve, reject) => {
-    createDMG({
-      appPath: `${DIST_PATH}/app/PostIgnis-darwin-x64/PostIgnis.app`,
-      name: 'PostIgnis',
-      overwrite: true,
-      icon: `${APP_DIST_PATH}/assets/icons/app.icns`,
-      out: `${DIST_PATH}/app/installer`
-    }, (error) => {
-      if (error) {
-        return reject(error);
-      }
+task('create-installer', async () => {
+  switch (process.platform) {
+    case 'darwin':
+      return new Promise((resolve, reject) => {
+        createDMG({
+          appPath: `${DIST_PATH}/app/PostIgnis-win32-x64/PostIgnis.app`,
+          name: 'PostIgnis',
+          overwrite: true,
+          icon: `${APP_DIST_PATH}/assets/icons/app.icns`,
+          out: `${DIST_PATH}/app/installer`
+        }, (error) => {
+          if (error) {
+            return reject(error);
+          }
 
-      return resolve();
-    });
-  });
+          return resolve();
+        });
+      });
+    case 'win32':
+      return electronInstaller.createWindowsInstaller({
+        appDirectory: `${DIST_PATH}/app/PostIgnis-win32-x64`,
+        outputDirectory: `${DIST_PATH}/app/installer`,
+        icon: `${APP_DIST_PATH}/assets/icons/app.ico`,
+        exe: 'PostIgnis.exe'
+      });
+  }
+});
 
-
-  return await mac;
-};
-
-exports.build = build;
-exports.copy = copy;
-exports.pack = pack;
-exports.installer = installer;
-
-exports.default = series(build, copy, pack);
+task('build-app', series('build-ui', 'copy-resources'));
+task('package', series('build-app', 'package-app'));
+task('installer', series('package', 'create-installer'));
